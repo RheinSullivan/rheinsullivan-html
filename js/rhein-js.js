047d4569e92230cd8aad747a7aeab70cbffd2e40
Rhein Sullivan <126785035+RheinSullivan@users.noreply.github.com>
2023-05-04 17:05:30 +0700

    Add files via upload

---
+++ js/rhein-js.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Game Deveoper", "Web Design", "3D Modeller", "Web Developer", "Youtuber", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
