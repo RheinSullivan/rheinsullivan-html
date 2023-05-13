2b0d1dce0306c0b905ea5b2058c73bc4e6a8da77
Rhein Sullivan <126785035+RheinSullivan@users.noreply.github.com>
2023-05-14 01:01:29 +0700

    Add files via upload

---
+++ js/rhein-js.js
@@ -0,0 +1,40 @@
+/* Toggle Icon Navbar */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+/* Scroll Active */
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    /* Sticky Navbar */
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+    /* Remove Toggle Icon and Navbar */
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+var typed = new Typed(".multiple-text",{
+    strings: ["Game Developer", "Web Design", "3D Modeller", "Web Developer", "Youtuber", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
